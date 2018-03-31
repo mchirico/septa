42c46666c4f9c7cd811a7c5279ed0646fe26edab
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-03-31 13:55:11 -0400

    Updated tests

--- cleanUp.sh
+++ cleanUp.sh
@@ -2,3 +2,9 @@
 # Quick formatting tool
 
 find . -iname '*.go' -exec gofmt -w {} \;
+
+# Tests
+echo "Running tests..... "
+go test github.com/mchirico/septa/firebase
+go test github.com/mchirico/septa/utils
+
