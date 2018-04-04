ef681c9ce93f15cc088cf21ce8f72a34c38c83e4
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-04-04 18:49:17 -0400

    Added simple test

--- utils/definitions_test.go
+++ utils/definitions_test.go
@@ -1,36 +1,65 @@
 package utils
 
 import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"log"
+	"strings"
 	"testing"
-	"time"
-	"fmt"
 )
 
+func fixtureReader(file string) *json.Decoder {
 
-func testSetup() []map[string]interface{}{
+	dat, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonStream := string(dat)
 
-	var testVariables []map[string]interface{}
-
-	loc, _ := time.LoadLocation("America/New_York")
-	tn := time.Now().In(loc)
-
-	trainRRSchedules := TrainRRSchedules{ TrainID:"TrainID",
-	DocDate:"DocDate",Timestamp: tn,
-	RRSchedules:nil}
-
-	fmt.Printf("%v\n",
-		trainRRSchedules.TrainID)
-
-
-	return testVariables
-
-
+	return json.NewDecoder(strings.NewReader(jsonStream))
 
 }
 
-
 func TestDefinitions(t *testing.T) {
 
-	testSetup()
+	fixtureReader("../fixtures/trainView.json")
+	status := []bool{false, false, false}
+
+	dat, err := ioutil.ReadFile("../fixtures/trainView.json")
+	assert.Nil(t, err)
+	jsonStream := string(dat)
+
+	dec := json.NewDecoder(strings.NewReader(jsonStream))
+	print(dec)
+
+	_, err = dec.Token()
+	assert.Nil(t, err)
+
+	for dec.More() {
+		var m LiveViewMessage
+		err := dec.Decode(&m)
+		assert.Nil(t, err)
+		if m.Lat == "39.959606265" {
+			status[0] = true
+		}
+		if m.Lon == "-75.063539333333" {
+			status[1] = true
+		}
+		if m.Source == "Marcus Hook" {
+			status[2] = true
+		}
+
+	}
+	for _, v := range status {
+		assert.True(t, v)
+	}
+
+	_, err = dec.Token()
+	assert.Nil(t, err)
+
+}
+
+func TestRRSchedulesType(t *testing.T) {
 
 }
