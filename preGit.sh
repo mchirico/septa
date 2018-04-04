183d143c4991110b2bd2a0a175466af9ff3679ca
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-04-04 08:36:36 -0400

    Added environment variables

--- preGit.sh
+++ preGit.sh
@@ -5,8 +5,8 @@ find . -iname '*.go' -exec gofmt -w {} \;
 
 # Tests
 echo "Running tests..... "
-go test -coverprofile=coverage.txt -covermode=atomic github.com/mchirico/septa/firebase
-go test -coverprofile=coverage.txt -covermode=atomic github.com/mchirico/septa/utils
+go test -cover fmt -covermode=atomic github.com/mchirico/septa/firebase
+go test -cover fmt -covermode=atomic github.com/mchirico/septa/utils
 go test -cover fmt
 echo -e 'Want to test build?\n\n   go build github.com/mchirico/septa/routefirebase \n\n'
 
