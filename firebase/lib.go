245fef76fe8c81de9166f6cccd3d6b259674b212
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-04-12 07:49:58 -0400

    Removed code not used

--- firebase/lib.go
+++ firebase/lib.go
@@ -139,44 +139,6 @@ func GetAllStationsRecordsWrapper(number int) []map[string]string {
 	return groupOfRecords
 }
 
-func deleteCollection(ctx context.Context, client *firestore.Client,
-	ref *firestore.CollectionRef, batchSize int) error {
-
-	for {
-		// Get a batch of documents
-		iter := ref.Limit(batchSize).Documents(ctx)
-		numDeleted := 0
-
-		// Iterate through the documents, adding
-		// a delete operation for each one to a
-		// WriteBatch.
-		batch := client.Batch()
-		for {
-			doc, err := iter.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				return err
-			}
-
-			batch.Delete(doc.Ref)
-			numDeleted++
-		}
-
-		// If there are no documents to delete,
-		// the process is over.
-		if numDeleted == 0 {
-			return nil
-		}
-
-		_, err := batch.Commit(ctx)
-		if err != nil {
-			return err
-		}
-	}
-}
-
 // DeleteDocument -- simple document delete
 func DeleteDocument(collection string, document string) {
 
